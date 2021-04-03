2b87d97910dd812d9b277df335548c0c20704651
Anmol Jindal <anmoljindal7982@gmail.com>
2021-04-03 21:58:55 +0530

    Now this change made by X

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 import pytest
 
 #for getting thebrowser name from command prompt
+
+# Change made by Y
+# Now thi sis change made by X
+
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="Chrome"
@@ -9,11 +13,10 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def setup(request):
-     
+
     driver=webdriver.Chrome(executable_path="E:\Python\Selenium\chromedriver.exe")
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     request.cls.driver=driver
     yield
     driver.close()
-    
