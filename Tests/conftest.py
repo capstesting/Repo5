ae26c647845a3c67d878f43b99662029f7be880b
Anmol Jindal <anmoljindal7982@gmail.com>
2021-04-03 22:56:48 +0530

    Change made in develop branch by X

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,23 +2,19 @@ from selenium import webdriver
 import pytest
 
 #for getting thebrowser name from command prompt
-
-# Change made by Y
-# Now thi sis change made by X
-
-# This line should cause conflict. Change made by X
-# This is an extra line
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="Chrome"
     )
 
+# Change made in Develop branch
 @pytest.fixture(scope="class")
 def setup(request):
-
+     
     driver=webdriver.Chrome(executable_path="E:\Python\Selenium\chromedriver.exe")
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     request.cls.driver=driver
     yield
     driver.close()
+    
